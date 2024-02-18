b25ba5c9eaff91e392083254100481fed976caf8
dani-mira <daniela.miranda.f@usach.cl>
2024-02-18 17:31:56 -0300

    Added tab wrappers for tab bar functionality, updated .gitignore

--- classes/window.py
+++ classes/window.py
@@ -1,7 +1,8 @@
-from PyQt6.QtWidgets import QWidget, QApplication
+import PyQt6.QtWidgets as QtWid
+from PyQt6.QtCore import Qt
 
 
-class App(QApplication):
+class App(QtWid.QApplication):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -25,6 +26,7 @@ class App(QApplication):
                                       id,
                                       title=title,
                                       size=size)
+
         return self.windows[id]
 
     # ## execution and wrapper routines
@@ -45,9 +47,9 @@ class App(QApplication):
 # ### Window handlers
 
 
-class MainWindow(QWidget):
+class MainWindow(QtWid.QWidget):
 
-    def __init__(self, parent: App, id, *, title="Default", size=(800, 600)):
+    def __init__(self, parent: App, id, *, title="Default", size=(800, 600), layout=None):
 
         super().__init__()
 
@@ -55,6 +57,13 @@ class MainWindow(QWidget):
         # this is a crime against OOP
 
         self.__parent = parent  # NEVER SHOULD BE CHANGED
+
+        if layout:
+            self.layout = layout
+        else:
+            self.layout = QtWid.QGridLayout(self)
+
+        self.setLayout(self.layout)
 
         self._id = id
         self.setObjectName(self._id)
@@ -64,6 +73,14 @@ class MainWindow(QWidget):
 
         self.title = self._title
         self.size = self._size
+
+    # ## static methods
+
+    def add_widget(self, wg, *, row, column, alignment=Qt.AlignmentFlag(0)):
+        # lets each widget wrapper decide how to initialise its subwidgets
+        wg._add_widgets()
+
+        self.layout.addWidget(wg, row, column, alignment)
 
     # ## Dynamic UI setting by simple dot notation access
     # i.e "window.title = 'Foo'" will directly alter the window title being displayed
@@ -108,3 +125,45 @@ class MainWindow(QWidget):
     def size(self, size):
         self._size = size
         self.resize(*size)
+
+
+# ### Tab handlers
+# parent corresponds to the window these reside in, both QTabWidget and QWidget must take the main window as their parent
+
+class TabWrapper(QtWid.QTabWidget):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.__parent = parent
+
+        self.subwidgets = {}
+
+    def add_tab(self, name, *, layout=None):
+
+        tab = Tab(self.__parent, name=name, layout=layout)
+        self.subwidgets[name] = tab
+
+        return tab
+
+    # personalised method for adding the subwidgets to the tab
+    def _add_widgets(self):
+
+        for name, widget in self.subwidgets.items():
+            self.addTab(widget, name)
+
+
+class Tab(QtWid.QWidget):
+
+    def __init__(self, parent, *, name, layout):
+        super().__init__(parent)
+
+        if layout:
+            self.layout = layout
+        else:
+            self.layout = QtWid.QGridLayout(self)
+
+        self.setLayout(self.layout)
+
+        self.name = name
+
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import sys
+import configparser
 
-from classes.window import App
+from classes.window import App, TabWrapper
+
+import PyQt6.QtWidgets as QtWid
 
 if __name__ == '__main__':
+
+    # ### Initialisation
 
     app = App(sys.argv)
 
     win = app.add_window("mainWindow",
                          title="Art Manager")
+
+    # tab handling
+
+    tabs = TabWrapper(win)
+
+    tabs.add_tab("Queue")
+    tabs.add_tab("Clients")
+    tabs.add_tab("Gallery")
+
+    win.add_widget(tabs,
+                   row=0,
+                   column=0)
 
     app.run()
